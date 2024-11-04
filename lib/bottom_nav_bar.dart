a9aa382cfedcd0cbabfdb89636a6a413f931cc16
axltavideus <yeetna01@gmail.com>
2024-11-04 22:30:13 +0700

    navbar bener

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -1,44 +1,48 @@
-// bottom_nav_bar.dart
-
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class BottomNavBar extends StatelessWidget {
   final int selectedIndex;
   final Function(int) onTap;
 
-  BottomNavBar({required this.selectedIndex, required this.onTap});
+  const BottomNavBar({super.key, required this.selectedIndex, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: selectedIndex,
       onTap: onTap,
+      backgroundColor: const Color.fromARGB(255, 56, 68, 244),
+      selectedItemColor: Colors.white, 
+      unselectedItemColor: Colors.white,
+      type: BottomNavigationBarType.fixed,
       items: [
         BottomNavigationBarItem(
           icon: Icon(
-            Icons.home,
-            color: selectedIndex == 0 ? Colors.blue : Colors.grey,
+            selectedIndex == 0 ? Icons.home : Icons.home_outlined,
           ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
           icon: Icon(
-            Icons.payment,
-            color: selectedIndex == 1 ? Colors.blue : Colors.grey,
+            selectedIndex == 1 ? Iconsax.money_35 : Iconsax.money_3,
           ),
           label: 'Pay',
         ),
         BottomNavigationBarItem(
-          icon: Icon(
-            Icons.history,
-            color: selectedIndex == 2 ? Colors.blue : Colors.grey,
+          icon: SizedBox(
+            width: 24, // Set a fixed width
+            height: 24, // Set a fixed height
+            child: Icon(
+              selectedIndex == 2 ? Iconsax.receipt_15 : Iconsax.receipt,
+              size: 24, // Ensure consistent size
+            ),
           ),
           label: 'History',
         ),
         BottomNavigationBarItem(
           icon: Icon(
-            Icons.person,
-            color: selectedIndex == 3 ? Colors.blue : Colors.grey,
+            selectedIndex == 3 ? Icons.person : Icons.person_outline,
           ),
           label: 'Profile',
         ),
