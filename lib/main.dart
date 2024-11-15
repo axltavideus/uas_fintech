acf16a1bf87d342ad39ad6d30093aedc84a982e9
unknown <David.Matthew.luisan@gmail.com>
2024-11-15 22:44:26 +0700

    ;

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:uas_fintech/camera_page.dart';
 import 'home_page.dart';
@@ -7,6 +9,19 @@ import 'history_page.dart';
 import 'profile.dart';
 
 void main() async {
+   if(kIsWeb){
+    await Firebase.initializeApp(
+    options: const FirebaseOptions(
+    apiKey: "AIzaSyDvZXoFhB6XQfOaRn1Pg6Xns3spbOOqfhU",
+    authDomain: "fire-setup-c3510.firebaseapp.com",
+    projectId: "fire-setup-c3510",
+    storageBucket: "fire-setup-c3510.firebasestorage.app",
+    messagingSenderId: "409745870124",
+    appId: "1:409745870124:web:9b0e269967a70c593bb089"
+  ));
+  } else{
+    await Firebase.initializeApp();
+  }
   WidgetsFlutterBinding.ensureInitialized();
   await _checkPermissions();
   runApp(MyApp());
