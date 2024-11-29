e4aba8fd5330e454058db23d9d887fbe8b34e8f4
RandyParker7 <randyestevan887@gmail.com>
2024-11-29 20:24:55 +0700

    menambahkan fungsi untruk mengecek user sudah login atau belum, jika belum akan di alihkan ke landing page. dan menambahkan icon logo mobile apps ke paltform ios

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,18 @@ import 'topup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'login.dart';
 import 'landing_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await _checkPermissions();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  
+  // Cek status login pengguna
+  User? user = FirebaseAuth.instance.currentUser;
+  bool isLoggedIn = user != null; // Determine if user is logged in
+  
+  runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
 // Function to check and request camera permissions
@@ -25,7 +32,10 @@ Future<void> _checkPermissions() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+
+  // Constructor now accepts isLoggedIn
+  const MyApp({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
@@ -35,18 +45,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // Set the initial route to the PinCodeWidget
-      initialRoute: '/pin',
+      // Set the initial route based on login status
+      initialRoute: isLoggedIn ? '/pin' : '/landing', // If logged in, go to pin, else go to landing page
       // Define the available routes
       routes: {
         '/pin': (context) => PinCodeWidget(), // Pin code entry page
         '/home': (context) => HomePage(), // Home page after PIN is entered
         '/profile': (context) => ProfilePage(),
         '/history': (context) => HistoryPage(),
-        '/topup': (context) => TopUpPage(), // Add this line
+        '/topup': (context) => TopUpPage(),
         '/pay': (context) => CameraPage(),
         '/login': (context) => LoginPage(),
-        '/landing': (context) => LandingPage(),
+        '/landing': (context) => LandingPage(), // Landing page for non-logged-in users
       },
     );
   }
